73843c7c5d48a8deeeda50c7f7ff22824488c2eb
pascaljesenberger <pascal@jesenberger.fr>
2025-08-03 16:11:59 +0200

    Add product service

--- Joiefull/Services/ProductService.swift
+++ Joiefull/Services/ProductService.swift
@@ -4,3 +4,32 @@
 //
 //  Created by Pascal Jesenberger on 03/08/2025.
 //
+
+import Foundation
+
+class ProductService {
+    static let shared = ProductService()
+
+    func fetchProducts(completion: @escaping (Result<[Product], Error>) -> Void) {
+        guard let url = URL(string: "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/Cr-ez-une-interface-dynamique-et-accessible-avec-SwiftUI/main/api/clothes.json") else {
+            return completion(.failure(URLError(.badURL)))
+        }
+
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                return completion(.failure(error))
+            }
+
+            guard let data = data else {
+                return completion(.failure(URLError(.badServerResponse)))
+            }
+
+            do {
+                let products = try JSONDecoder().decode([Product].self, from: data)
+                completion(.success(products))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
