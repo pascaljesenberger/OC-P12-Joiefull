6dac5d0870eb7cc12e6f20587dd6c5d552410589
pascaljesenberger <pascal@jesenberger.fr>
2025-08-03 16:11:01 +0200

    Add product structure

--- Joiefull/Models/Product.swift
+++ Joiefull/Models/Product.swift
@@ -4,3 +4,27 @@
 //
 //  Created by Pascal Jesenberger on 03/08/2025.
 //
+
+import Foundation
+
+struct Product: Identifiable, Decodable {
+    let id: Int
+    let picture: Picture
+    let name: String
+    let category: Category
+    let likes: Int
+    let price: Double
+    let original_price: Double
+}
+
+struct Picture: Decodable {
+    let url: String
+    let description: String
+}
+
+enum Category: String, Decodable, CaseIterable {
+    case accessories = "ACCESSORIES"
+    case bottoms = "BOTTOMS"
+    case shoes = "SHOES"
+    case tops = "TOPS"
+}
