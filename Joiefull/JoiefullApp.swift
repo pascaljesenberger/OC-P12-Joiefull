8bb70332183c39b6a2417f58ecba83708084e017
pascaljesenberger <pascal@jesenberger.fr>
2025-08-03 13:47:05 +0200

    Initial Commit

---
+++ Joiefull/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Pascal Jesenberger on 03/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JoiefullTests/JoiefullTests.swift
@@ -0,0 +1,16 @@
+//
+//  JoiefullTests.swift
+//  JoiefullTests
+//
+//  Created by Pascal Jesenberger on 03/08/2025.
+//
+
+import Testing
+
+struct JoiefullTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
