1733cf3c0ab81e8371505df18e20828234c7e311
pascaljesenberger <pascal@jesenberger.fr>
2025-08-18 18:43:43 +0200

    Add textfield

--- Joiefull/Views/Components/Product/ProductRating.swift
+++ Joiefull/Views/Components/Product/ProductRating.swift
@@ -11,22 +11,37 @@ struct ProductRating: View {
     @Environment(\.sizeCategory) private var sizeCategory
     private let device = UIDevice.current
     @State private var rating: Int = 0
+    @State private var reviewText = ""
     
     var body: some View {
-        HStack {
-            Image(systemName: "person.crop.circle")
-                .font(.system(size: ResponsiveSizes.fontSize(42, for: sizeCategory, device: device)))
-                .foregroundColor(Color.blue)
-            
-            ForEach(1...5, id: \.self) { index in
-                Image(systemName: index <= rating ? "star.fill" : "star")
-                    .font(.system(size: ResponsiveSizes.fontSize(28, for: sizeCategory, device: device)))
-                    .foregroundColor(index <= rating ? Color.appOrange : .black.opacity(0.7))
-                    .onTapGesture {
-                        rating = index
-                    }
+        VStack(spacing: 24) {
+            HStack {
+                Image(systemName: "person.crop.circle")
+                    .font(.system(size: ResponsiveSizes.fontSize(42, for: sizeCategory, device: device)))
+                    .foregroundColor(Color.blue)
+                
+                ForEach(1...5, id: \.self) { index in
+                    Image(systemName: index <= rating ? "star.fill" : "star")
+                        .font(.system(size: ResponsiveSizes.fontSize(28, for: sizeCategory, device: device)))
+                        .foregroundColor(index <= rating ? Color.appOrange : .black.opacity(0.5))
+                        .onTapGesture {
+                            rating = index
+                        }
+                }
             }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            TextField("Partagez ici votre impression sur cette pièce", text: $reviewText, axis: .vertical)
+                .lineLimit(3...10)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.black, lineWidth: 1)
+                        .opacity(0.2)
+                )
+                .submitLabel(.done)
         }
+        .padding(.horizontal)
     }
 }
 
